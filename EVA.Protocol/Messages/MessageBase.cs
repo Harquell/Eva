9900284d7e9a72eab933f5f99b79458291f6da6a
Harquel Loïc <dayosdr@gmail.com>
2019-12-27 08:25:37 +0100

    Add project Protocol & Common

---
+++ EVA.Protocol/Messages/MessageBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EVA.Protocol.Messages
+{
+    public interface IMessageBase
+    {
+        public abstract ushort PacketId { get; }
+
+        public abstract byte[] Serialize();
+
+        protected abstract IMessageBase Deserialize();
+    }
+
+    public abstract class MessageBase<T> : IMessageBase where T : IMessageBase
+    {
+        public abstract ushort PacketId { get; }
+
+        protected abstract T Deserialize();
+
+        public abstract byte[] Serialize();
+
+        IMessageBase IMessageBase.Deserialize()
+            => Deserialize();
+    }
+}
